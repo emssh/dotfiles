fb977e6f65dcbd40666a3df3abe687f377f43924
emmad <emmadgilani11@gmail.com>
2026-03-15 17:31:41 -0400

    Adding install script & package lists

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+
+echo "==> Installing stow..."
+sudo pacman -S --needed stow
+
+echo "==> Stowing packages..."
+cd "$(dirname "$0")"
+
+PACKAGES=(
+    zsh
+    hypr
+    waybar
+    kitty
+    rofi
+    nwg-look
+    thunar
+)
+
+for pkg in "${PACKAGES[@]}"; do
+    echo "  stowing $pkg..."
+    stow "$pkg"
+done
+
+echo "==> Restoring packages..."
+sudo pacman -S --needed - < pkglist.txt
+
+echo "==> Installing AUR helper..."
+if ! command -v yay &> /dev/null; then
+    sudo pacman -S --needed git base-devel
+    git clone https://aur.archlinux.org/yay.git /tmp/yay
+    cd /tmp/yay && makepkg -si
+    cd "$(dirname "$0")"
+fi
+
+echo "==> Restoring AUR packages..."
+yay -S --needed - < pkglist-aur.txt
+
+echo "==> Done! Run: exec \$SHELL"
